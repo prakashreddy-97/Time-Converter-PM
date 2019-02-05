1128a5f363f25a73028389276d7832a27d42cb12
Mamidi,Sai Prakash Reddy <s533725@ch140014m.nwmissouri.edu>
2019-02-05 12:49:57 -0600

    Initial Commit

---
+++ Time Converter PM/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Time Converter PM
+//
+//  Created by Mamidi,Sai Prakash Reddy on 2/5/19.
+//  Copyright © 2019 Mamidi,Sai Prakash Reddy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
